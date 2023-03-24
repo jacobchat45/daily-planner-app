a243558f02767d6e01b3b4244d26ff013b3c48a9
Jacob Chatfield <jacob.chatfield45@outlook.com>
2023-03-24 19:43:56 +0000

    Adding code to make timeblocks and elements for the timeblock

--- starter/index.js
+++ starter/index.js
@@ -4,22 +4,38 @@ $(document).ready(function () {
     let current = moment().format("dddd Do MMMM, YYYY");
     $("#currentDay").text(current);
 
-    // Define an array to store the time values for each hour
+    // making an array so we can store the time 
     let hrs = [];
 
-    // Loop through the hours from 9am to 5pm and format them
+    // Loop through the hours from 9am to 5pm and then format them
     for (let i = 9; i <= 17; i++) {
         let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
         hrs.push(hour);
     }
 
-    // Declare a variable to store the timeout for the save message
+    
+    for (let i = 9; i <= 17; i++) {
+        let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
+        hrs.push(hour);
+    };
+
+    // Declaring a variable to store the timeout for the save message
     let timeout;
-    // Create a save message element
-    let saveMessage = $("<p>").addClass("message").text("Your actvity has been saved");
+    // making a save message element
 
-    for (let i = 9; i <= 17; i++) {
-        let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
-        hrs.push(hour);
-    }
+    let saveMessage = $("<p>").addClass("message").text("Activity saved!");
+
+    // looping thro hours and creating blocks for each hour
+
+ hrs.forEach((hour, index) => {
+    // Creating timeblock element
+    let timeBlock = $("<div>").addClass("time-block row");
+    // Creating hour element
+    let hourElement = $("<div>").addClass("hour col-2").text(hour);
+    // Creating input element for the schedule
+    let scheduleInput = $("<input>")
+        .addClass("scheduleInput col-8")
+        .attr("id", `input-${index}`);
+         // Set a unique id for each input
+
 });
